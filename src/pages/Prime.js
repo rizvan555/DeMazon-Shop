28cdbebb3d6ceb28d92338eea377f709b19a7c5e
RizvanKarimov <rizvan@AirvonSperCoder.fritz.box>
2023-04-17 15:25:49 +0200

    porject finished without style

--- src/pages/Prime.js
+++ src/pages/Prime.js
@@ -4,7 +4,75 @@ const Prime = () => {
   return (
     <div>
       <Header />
-      <h1>Prime</h1>
+      <div
+        className="prime-box"
+        style={{
+          padding: "30px",
+        }}
+      >
+        <h1>Prime</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+          necessitatibus animi nostrum natus numquam, cum, fuga repudiandae
+          laboriosam, iste consequuntur voluptates! Esse dicta et fugiat facilis
+          voluptatum ab. Quaerat modi velit accusantium voluptatibus non
+          architecto perspiciatis, autem incidunt deserunt dolorem aliquid
+          ducimus placeat explicabo odit repellat, perferendis pariatur
+          laudantium inventore distinctio accusamus cum aliquam ipsam! Nulla
+          tempora placeat asperiores porro reprehenderit cum perspiciatis ipsum
+          mollitia dolor dolorum hic, incidunt distinctio expedita quaerat
+          voluptatum, at libero ipsa et, maiores quia consectetur omnis commodi
+          autem! Exercitationem earum laboriosam officiis expedita in
+          perferendis amet, maxime sequi optio voluptatem laborum veritatis
+          minus, ea nesciunt quam atque cum quasi repudiandae iusto? Vel
+          maiores, minima ullam laudantium magni modi accusamus, autem debitis
+          nulla, libero veritatis quaerat dolorum iure! A nobis tempora soluta
+          vitae iure magnam pariatur quo ipsam odit in nemo eius, praesentium
+          consequatur facere non maiores recusandae natus, eveniet et ullam
+          aspernatur sapiente accusamus aliquam. Sint, distinctio sequi, rerum
+          architecto doloribus eveniet ratione consequuntur modi recusandae
+          facilis eius ad eos ea tempora reprehenderit, nihil quibusdam culpa
+          aliquid natus porro numquam! Veniam praesentium esse dolorem!
+          Voluptate tempore ducimus odio? Ad corporis dolorum sapiente
+          laudantium distinctio inventore in aspernatur enim quod ullam neque
+          mollitia eaque doloribus sunt ratione eos, officiis dolorem nostrum?
+          Vitae tempore illo doloribus vero, quis architecto. Saepe ullam odit
+          sit eaque, neque itaque sed unde pariatur minus modi hic sapiente
+          aliquam dolores, aperiam doloribus nihil ipsa eos exercitationem
+          obcaecati? Distinctio sunt ab corporis labore earum cupiditate
+          blanditiis quo saepe, ipsum non totam magni ullam quasi velit et
+          necessitatibus maiores voluptatum incidunt perferendis, numquam
+          possimus illo iure molestias esse? Repudiandae voluptatum
+          reprehenderit magnam ad fugiat inventore iusto nostrum odio quasi,
+          quaerat impedit reiciendis iure quas rem, totam ea et exercitationem,
+          at possimus fuga. Reiciendis corporis iste deserunt quibusdam mollitia
+          aperiam nulla, accusantium modi quas officia iure, sed eos est hic
+          delectus laboriosam rerum ab perferendis libero suscipit. Iste,
+          numquam. Praesentium deserunt omnis quia ab, rerum ratione tempora
+          accusamus mollitia necessitatibus sed corporis sequi expedita dolorum
+          fugiat aspernatur, at incidunt recusandae aperiam possimus non.
+          Eveniet, asperiores iste harum enim hic neque rem odio, nihil esse
+          repellat blanditiis molestiae vel cumque reprehenderit et voluptatibus
+          tempore accusamus ipsa? Error magnam quaerat, fugit eos optio,
+          suscipit, atque minima repellat aliquam incidunt perspiciatis porro.
+          Voluptatum mollitia saepe aperiam facilis veritatis perferendis iure
+          deleniti quod, ipsam incidunt explicabo, corporis voluptates
+          molestiae. Quisquam necessitatibus, expedita voluptas autem quibusdam
+          atque suscipit quae incidunt explicabo excepturi delectus officiis
+          alias ab! Odio, aliquam. Rerum dicta molestias dolorum quaerat at
+          ducimus ipsum. Accusantium eveniet nisi provident perspiciatis ipsam
+          accusamus, blanditiis minima libero inventore corporis numquam quam
+          minus veritatis officia hic reprehenderit! Recusandae aspernatur a,
+          enim reprehenderit quis pariatur in consequatur iure magni porro
+          tenetur incidunt obcaecati. Perferendis minima consequatur quod hic
+          reiciendis quo? Laboriosam repellat unde numquam commodi porro
+          voluptate officia voluptatibus! Repellat illum deleniti temporibus
+          nihil provident impedit facere quia sequi tempore. Deleniti
+          repellendus consequuntur hic illo optio nostrum, magnam eos quibusdam?
+          Fuga aut, tempore minus obcaecati cum, aspernatur, nihil porro sed
+          voluptas deserunt molestiae.
+        </p>
+      </div>
     </div>
   );
 };
