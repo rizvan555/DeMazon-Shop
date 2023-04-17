c18af975ed8d338e171e7e82ab386e48df90dce5
RizvanKarimov <rizvan@AirvonSperCoder.fritz.box>
2023-04-17 17:00:27 +0200

    style 60% finished

--- src/pages/Bestseller.js
+++ src/pages/Bestseller.js
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import ProductList from "../components/ProductList";
+import styled from "styled-components";
+
+const BestsellerStyled = styled.div`
+  background: rgb(238, 174, 202);
+  background: radial-gradient(
+    circle,
+    rgba(238, 174, 202, 1) 0%,
+    rgba(148, 187, 233, 1) 100%
+  );
+`;
 
 const Bestseller = (props) => {
   return (
-    <>
+    <BestsellerStyled>
       <Header />
       <ProductList products={props.products} />
-    </>
+    </BestsellerStyled>
   );
 };
 
